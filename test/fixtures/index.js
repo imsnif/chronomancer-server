e35e315b47a3aa7677146b9ee57b1cb92fbd673b
Aram Drevekenin <grimsniffer@gmail.com>
2017-07-06 15:15:27 +0200

    refactor: tests

---
+++ test/fixtures/index.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const r = require('rethinkdb')
+const mockData = require('./mock-data')
+
+function insertData (name, data, connection) {
+  return new Promise((resolve, reject) => {
+    r.db('chronomancer').table(name).insert(data).run(connection, (err, result) => {
+      if (err) return reject(err)
+      resolve()
+    })
+  })
+}
+
+function getConnection () {
+  return new Promise((resolve, reject) => {
+    r.connect((err, connection) => {
+      if (err) return reject(err)
+      resolve(connection)
+    })
+  })
+}
+
+function dropDatabase (name, connection) {
+  return new Promise((resolve, reject) => {
+    r.dbDrop('chronomancer').run(connection, (err, result) => {
+      if (err) return reject(err)
+      resolve(result)
+    })
+  })
+}
+
+function createDatabase (name, connection) {
+  return new Promise((resolve, reject) => {
+    r.dbCreate('chronomancer').run(connection, (err, result) => {
+      if (err) return reject(err)
+      resolve(result)
+    })
+  })
+}
+
+function createTable (name, connection) {
+  return new Promise((resolve, reject) => {
+    r.db('chronomancer').tableCreate(name).run(connection, (err, result) => {
+      if (err) return reject(err)
+      resolve(result)
+    })
+  })
+}
+
+module.exports = async function fixtures () {
+  const tables = [ 'players', 'powers', 'timelines' ]
+  const connection = await getConnection()
+  await dropDatabase('chronomancer', connection)
+  await createDatabase('chronomancer', connection)
+  await Promise.all(tables.map(tableName => createTable(tableName, connection)))
+  await Promise.all(tables.map(tableName => insertData(tableName, mockData[tableName], connection)))
+  return connection
+}
--- test/timeline.spec.js
+++ test/timeline.spec.js
@@ -3,47 +3,18 @@
 const test = require('tape')
 const request = require('supertest')
 const r = require('rethinkdb')
-const mockData = require('./mock-data')
+const fixtures = require('./fixtures')
 
-async function insertData (name, data, connection) {
+function getPlayerItems (id, conn) {
   return new Promise((resolve, reject) => {
-    r.db('chronomancer').table(name).insert(data).run(connection, (err, result) => {
+    r.db('chronomancer').table('players').filter({id})('items').run(conn, (err, cursor) => {
       if (err) return reject(err)
-      resolve()
-    })
-  })
-}
-
-async function fixtures () {
-  const tables = [ 'players', 'powers', 'timelines' ]
-  const connection = await new Promise((resolve, reject) => {
-    r.connect((err, connection) => {
-      if (err) return reject(err)
-      resolve(connection)
-    })
-  })
-  await new Promise((resolve, reject) => {
-    r.dbDrop('chronomancer').run(connection, (err, result) => {
-      if (err) return reject(err)
-      r.dbCreate('chronomancer').run(connection, (err, result) => {
+      cursor.toArray((err, results) => {
         if (err) return reject(err)
-        resolve(Promise.all(
-          tables.map(tableName => {
-            return new Promise((resolve, reject) => {
-              r.db('chronomancer').tableCreate(tableName).run(connection, (err, result) => {
-                if (err) return reject(err)
-                return resolve()
-              })
-            })
-          })
-        ))
+        resolve(results[0][0])
       })
     })
   })
-  for (let tName of tables) {
-    await insertData(tName, mockData[tName], connection)
-  }
-  return connection
 }
 
 test('POST /timeline/quest/:timelineName => adds relevant item to player', async t => {
@@ -51,20 +22,11 @@ test('POST /timeline/quest/:timelineName => adds relevant item to player', async
   try {
     const conn = await fixtures()
     const app = require('../app')(conn)
-    await new Promise(resolve => setTimeout(resolve, 200)) // TODO: remove connection from app to fix this
     await request(app)
       .post('/timeline/quest/Timeline 1')
       .set('userId', 1)
       .expect(200)
-    const playerItems = await new Promise((resolve, reject) => {
-      r.db('chronomancer').table('players').filter({id: 1})('items').run(conn, (err, cursor) => {
-        if (err) throw err
-        cursor.toArray((err, results) => {
-          if (err) throw err
-          resolve(results[0][0])
-        })
-      })
-    })
+    const playerItems = await getPlayerItems(1, conn)
     t.deepEquals(playerItems, {source: 'Timeline 1', name: 'steal'}, 'Item added to player')
     conn.close()
   } catch (e) {
