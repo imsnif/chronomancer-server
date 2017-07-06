28f442afa37325403fe6e3018245eeaac7541b3b
Aram Drevekenin <grimsniffer@gmail.com>
2017-07-06 21:11:56 +0200

    style: function placement

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
 
+function errorHandler (err, req, res, next) {
+  res.send(res.statusCode || 500).json({error: err.msg})
+}
+
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(expressValidator())
 app.use(errorHandler)
-
-function errorHandler (err, req, res, next) {
-  res.send(res.statusCode || 500).json({error: err.msg})
-}
 
 module.exports = function (connection) {
   connection.use('chronomancer')
