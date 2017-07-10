1821b56011fbc973db53476a4d2bf3fa865f4287
Aram Drevekenin <grimsniffer@gmail.com>
2017-07-10 12:28:42 +0200

    feat: handle unlock power

--- jobs/handle.js
+++ jobs/handle.js
@@ -1,7 +1,10 @@
 'use strict'
 
 module.exports = function (connection) {
-  const { lockTimeline } = require('../service/timeline')(connection)
+  const {
+    lockTimeline,
+    unlockTimeline
+  } = require('../service/timeline')(connection)
   const { getPlayer } = require('../service/player')(connection)
   return {
     locking: async power => {
@@ -9,6 +12,15 @@ module.exports = function (connection) {
       const player = await getPlayer(power.playerId)
       if (player.items.map(i => i.name).includes('lock')) {
         return lockTimeline(power.timelineName)
+      } else {
+        return Promise.resolve()
+      }
+    },
+    unlocking: async power => {
+      if (!power || !power.timelineName) return Promise.resolve()
+      const player = await getPlayer(power.playerId)
+      if (player.items.map(i => i.name).includes('unlock')) {
+        return unlockTimeline(power.timelineName)
       } else {
         return Promise.resolve()
       }
