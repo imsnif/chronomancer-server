f784841d17aab69b8f3b86515d5b72a948cbbf1f
Aram Drevekenin <grimsniffer@gmail.com>
2017-07-15 19:27:00 +0200

    test: combine power resolution

---
+++ test/power-combine.spec.js
@@ -0,0 +1,233 @@
+'use strict'
+
+const test = require('tape')
+const fixtures = require('./fixtures')
+const jobsFactory = require('../jobs')
+const {
+  createPower,
+  getPower,
+  getPlayerItems
+} = require('./test-utils')
+
+test('Combine power resolution => success (assist, prevent)', async t => {
+  t.plan(2)
+  try {
+    const conn = await fixtures()
+    const jobs = jobsFactory(conn)
+    const timelineName = 'Timeline 8'
+    const playerId = 6
+    const now = new Date()
+    await createPower({
+      playerId,
+      gameId: 1,
+      timelineName,
+      name: 'Combining',
+      startTime: now.getTime(),
+      endTime: now.getTime(),
+      target: {
+        type: 'timeline',
+        name: timelineName,
+        itemName: 'lock'
+      },
+      allies: [],
+      enemies: []
+    }, conn)
+    await jobs()
+    const items = await getPlayerItems(playerId, conn)
+    const power = await getPower(playerId, timelineName, conn)
+    t.deepEquals(items, [
+      {name: 'assist', source: 'Timeline 2'},
+      {name: 'prevent', source: 'Timeline 2'},
+      {name: 'lock', source: 'Timeline 8'}
+    ], 'lock item added to player')
+    t.notOk(power, 'power was deleted')
+  } catch (e) {
+    console.error(e.stack)
+    t.fail(e.message)
+  }
+})
+
+test('Combine power resolution => success (reset, steal)', async t => {
+  t.plan(2)
+  try {
+    const conn = await fixtures()
+    const jobs = jobsFactory(conn)
+    const timelineName = 'Timeline 8'
+    const playerId = 7
+    const now = new Date()
+    await createPower({
+      playerId,
+      gameId: 1,
+      timelineName,
+      name: 'Combining',
+      startTime: now.getTime(),
+      endTime: now.getTime(),
+      target: {
+        type: 'timeline',
+        name: timelineName,
+        itemName: 'unlock'
+      },
+      allies: [],
+      enemies: []
+    }, conn)
+    await jobs()
+    const items = await getPlayerItems(playerId, conn)
+    const power = await getPower(playerId, timelineName, conn)
+    t.deepEquals(items, [
+      {name: 'assist', source: 'Timeline 2'},
+      {name: 'prevent', source: 'Timeline 2'},
+      {name: 'reset', source: 'Timeline 2'},
+      {name: 'steal', source: 'Timeline 2'},
+      {name: 'unlock', source: 'Timeline 8'}
+    ], 'lock item added to player')
+    t.notOk(power, 'power was deleted')
+  } catch (e) {
+    console.error(e.stack)
+    t.fail(e.message)
+  }
+})
+
+test('Combine power resolution => failure (negative score)', async t => {
+  t.plan(2)
+  try {
+    const conn = await fixtures()
+    const jobs = jobsFactory(conn)
+    const timelineName = 'Timeline 8'
+    const playerId = 6
+    const now = new Date()
+    await createPower({
+      playerId,
+      gameId: 1,
+      timelineName,
+      name: 'Combining',
+      startTime: now.getTime(),
+      endTime: now.getTime(),
+      target: {
+        type: 'timeline',
+        name: timelineName,
+        itemName: 'lock'
+      },
+      allies: [],
+      enemies: [{id: 1, score: 2}]
+    }, conn)
+    await jobs()
+    const items = await getPlayerItems(playerId, conn)
+    const power = await getPower(playerId, timelineName, conn)
+    t.deepEquals(items, [
+      {name: 'assist', source: 'Timeline 2'},
+      {name: 'prevent', source: 'Timeline 2'}
+    ], 'player items unchanged')
+    t.notOk(power, 'power was deleted')
+  } catch (e) {
+    console.error(e.stack)
+    t.fail(e.message)
+  }
+})
+
+test('Combine power resolution => failure (items no longer exists) (assist, prevent)', async t => {
+  t.plan(2)
+  try {
+    const conn = await fixtures()
+    const jobs = jobsFactory(conn)
+    const timelineName = 'Timeline 1'
+    const playerId = 1
+    const now = new Date()
+    await createPower({
+      playerId,
+      gameId: 1,
+      timelineName,
+      name: 'Combining',
+      startTime: now.getTime(),
+      endTime: now.getTime(),
+      target: {
+        type: 'timeline',
+        name: timelineName,
+        itemName: 'lock'
+      },
+      allies: [],
+      enemies: []
+    }, conn)
+    await jobs()
+    const items = await getPlayerItems(playerId, conn)
+    const power = await getPower(playerId, timelineName, conn)
+    t.deepEquals(items, [], 'player items unchanged')
+    t.notOk(power, 'power was deleted')
+  } catch (e) {
+    console.error(e.stack)
+    t.fail(e.message)
+  }
+})
+
+test('Combine power resolution => failure (one item no longer exists) (reset, steal)', async t => {
+  t.plan(2)
+  try {
+    const conn = await fixtures()
+    const jobs = jobsFactory(conn)
+    const timelineName = 'Timeline 8'
+    const playerId = 6
+    const now = new Date()
+    await createPower({
+      playerId,
+      gameId: 1,
+      timelineName,
+      name: 'Combining',
+      startTime: now.getTime(),
+      endTime: now.getTime(),
+      target: {
+        type: 'timeline',
+        name: timelineName,
+        itemName: 'unlock'
+      },
+      allies: [],
+      enemies: []
+    }, conn)
+    await jobs()
+    const items = await getPlayerItems(playerId, conn)
+    const power = await getPower(playerId, timelineName, conn)
+    t.deepEquals(items, [
+      {name: 'assist', source: 'Timeline 2'},
+      {name: 'prevent', source: 'Timeline 2'}
+    ], 'player items unchanged')
+    t.notOk(power, 'power was deleted')
+  } catch (e) {
+    console.error(e.stack)
+    t.fail(e.message)
+  }
+})
+
+test('Combine power resolution => failure (player no longer in timeline)', async t => {
+  t.plan(2)
+  try {
+    const conn = await fixtures()
+    const jobs = jobsFactory(conn)
+    const timelineName = 'Timeline 1'
+    const playerId = 6
+    const now = new Date()
+    await createPower({
+      playerId,
+      gameId: 1,
+      timelineName,
+      name: 'Combining',
+      startTime: now.getTime(),
+      endTime: now.getTime(),
+      target: {
+        type: 'timeline',
+        name: timelineName,
+        itemName: 'lock'
+      },
+      allies: [],
+      enemies: []
+    }, conn)
+    await jobs()
+    const items = await getPlayerItems(playerId, conn)
+    const power = await getPower(playerId, timelineName, conn)
+    t.deepEquals(items, [
+      {name: 'assist', source: 'Timeline 2'},
+      {name: 'prevent', source: 'Timeline 2'}
+    ], 'player items unchanged')
+    t.notOk(power, 'power was deleted')
+  } catch (e) {
+    console.error(e.stack)
+    t.fail(e.message)
+  }
+})
