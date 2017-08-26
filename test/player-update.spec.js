22c9cc0f0acff7e6afea8e0e5b2efc1ef67a40d7
Aram Drevekenin <grimsniffer@gmail.com>
2017-08-26 23:22:56 +0200

    test: stub passport

--- test/player-update.spec.js
+++ test/player-update.spec.js
@@ -1,18 +1,43 @@
 'use strict'
 
+const decache = require('decache')
 const test = require('tape')
 const request = require('supertest')
+const sinon = require('sinon')
 const fixtures = require('./fixtures')
 const { getPlayer } = require('./test-utils')
+const passport = require('passport')
+
+let origAuthenticate // https://github.com/sinonjs/sinon/issues/166
+
+function stubPassport (username, userpic, userId) {
+  decache('../app')
+  if (!origAuthenticate) {
+    origAuthenticate = passport.authenticate
+  }
+  passport.authenticate = origAuthenticate
+  sinon.stub(passport, 'authenticate').returns((req, res, next) => {
+    console.log('res.user.id:', userId)
+    req.user = {
+      id: userId,
+      photos: [{
+        value: userpic
+      }],
+      displayName: username
+    }
+    next()
+  })
+}
 
 test('POST /player/update => creates new player', async t => {
   t.plan(1)
   try {
-    const conn = await fixtures()
-    const app = require('../app')(conn)
     const username = 'foo'
     const userpic = 'bar'
     const userId = '42'
+    stubPassport(username, userpic, userId)
+    const conn = await fixtures()
+    const app = require('../app')(conn)
     await request(app)
       .post(`/player/update`)
       .set('userId', userId)
@@ -34,11 +59,12 @@ test('POST /player/update => creates new player', async t => {
 test('POST /player/update => updates existing player', async t => {
   t.plan(1)
   try {
-    const conn = await fixtures()
-    const app = require('../app')(conn)
     const username = 'foo'
     const userpic = 'bar'
     const userId = '1'
+    stubPassport(username, userpic, userId)
+    const conn = await fixtures()
+    const app = require('../app')(conn)
     await request(app)
       .post(`/player/update`)
       .set('userId', userId)
@@ -54,63 +80,6 @@ test('POST /player/update => updates existing player', async t => {
       items: [],
       actions: 10
     }, 'player updated with name and pic')
-  } catch (e) {
-    console.error(e.stack)
-    t.fail(e.message)
-  }
-})
-
-test('POST /player/update => bad parameters - no userId', async t => {
-  t.plan(1)
-  try {
-    const conn = await fixtures()
-    const app = require('../app')(conn)
-    const username = 'foo'
-    const userpic = 'bar'
-    await request(app)
-      .post(`/player/update`)
-      .set('userpic', userpic)
-      .set('name', username)
-      .expect(400)
-    t.pass('proper error message shown')
-  } catch (e) {
-    console.error(e.stack)
-    t.fail(e.message)
-  }
-})
-
-test('POST /player/update => bad parameters - no username', async t => {
-  t.plan(1)
-  try {
-    const conn = await fixtures()
-    const app = require('../app')(conn)
-    const userpic = 'bar'
-    const userId = '1'
-    await request(app)
-      .post(`/player/update`)
-      .set('userId', userId)
-      .set('userpic', userpic)
-      .expect(400)
-    t.pass('proper error message shown')
-  } catch (e) {
-    console.error(e.stack)
-    t.fail(e.message)
-  }
-})
-
-test('POST /player/update => bad parameters - no userpic (link)', async t => {
-  t.plan(1)
-  try {
-    const conn = await fixtures()
-    const app = require('../app')(conn)
-    const username = 'foo'
-    const userId = '1'
-    await request(app)
-      .post(`/player/update`)
-      .set('userId', userId)
-      .set('name', username)
-      .expect(400)
-    t.pass('proper error message shown')
   } catch (e) {
     console.error(e.stack)
     t.fail(e.message)
